d02c1c5f1f4ec36fce5beddc0e2237d34118225a
Jona Pierini <jonapierini@gmail.com>
2024-08-12 20:53:28 +0200

    modal and fetch

--- src/data/moviesSlice.js
+++ src/data/moviesSlice.js
@@ -1,27 +1,33 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-export const fetchMovies = createAsyncThunk('fetch-movies', async (apiUrl) => {
-    const response = await fetch(apiUrl)
-    return response.json()
-})
+export const fetchMovies = createAsyncThunk("fetch-movies", async () => {
+  const url =
+    "https://api.themoviedb.org/3/movie/popular?api_key=e7283079d2e17305df26fdfba2e3851d&page=1";
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+});
 
 const moviesSlice = createSlice({
-    name: 'movies',
-    initialState: { 
-        movies: [],
-        fetchStatus: '',
-    },
-    reducers: {},
-    extraReducers: (builder) => {
-        builder.addCase(fetchMovies.fulfilled, (state, action) => {
-            state.movies = action.payload
-            state.fetchStatus = 'success'
-        }).addCase(fetchMovies.pending, (state) => {
-            state.fetchStatus = 'loading'
-        }).addCase(fetchMovies.rejected, (state) => {
-            state.fetchStatus = 'error'
-        })
-    }
-})
+  name: "movies",
+  initialState: {
+    movies: [],
+    fetchStatus: "",
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMovies.fulfilled, (state, action) => {
+        state.movies = action.payload;
+        state.fetchStatus = "success";
+      })
+      .addCase(fetchMovies.pending, (state) => {
+        state.fetchStatus = "loading";
+      })
+      .addCase(fetchMovies.rejected, (state) => {
+        state.fetchStatus = "error";
+      });
+  },
+});
 
-export default moviesSlice
+export default moviesSlice;
